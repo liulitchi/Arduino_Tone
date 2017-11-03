b423a048cb55d27daed18162809488bd8f028918
liulitchi <kz-xy@163.com>
2017-11-03 22:06:03 +0800

    Create pitchesNMN.h

---
+++ pitchesNMN.h
@@ -0,0 +1,36 @@
+/*************************************************
+ * 作者: litchi 
+ * 参考资料: https://zh.wikipedia.org/wiki/%E5%8D%81%E4%BA%8C%E5%B9%B3%E5%9D%87%E5%BE%8B
+ * 简谱: 简谱符号 频率（Hz）
+ * 使用: 如 bd6表示倍低音mi; d2表示低音re; z1表示中音do；g4表示高音fa
+ *************************************************/
+//倍低音表示 cdn:
+#define bd3 330
+#define bd4 349
+#define bd5 392
+#define bd6 440
+#define bd7 494
+//低音表示 dn:
+#define d1  523
+#define d2  587
+#define d3  659
+#define d4  698
+#define d5  784
+#define d6  880
+#define d7  988
+//中音表示 zn:
+#define z1  1047
+#define z2  1175
+#define z3  1319
+#define z4  1397
+#define z5  1568
+#define z6  1760
+#define z7  1976
+//高音表示 gn:
+#define g1  2093
+#define g2  2349
+#define g3  2637
+#define g4  2794
+#define g5  3136
+#define g6  3520
+#define g7  3951
