5d5ceec74fa9e41cf76d7455c34c986cf9bad7ea
liulitchi <kz-xy@163.com>
2017-11-03 22:13:56 +0800

    Add files via upload

---
+++ pitches12ET.h
@@ -0,0 +1,100 @@
+/*************************************************
+ * Public Constants
+ * 作者: Brett Hagman
+ * 来自: https://www.arduino.cc/en/Tutorial/ToneMelody?from=Tutorial.Tone
+ **************************************************
+ * 注释: litchi 
+ * 蜂鸣器频率范围(数据存疑)： 有源 300~2300Hz , 无源 1900~4000Hz 
+ * 十二平均律：名称 频率（Hz） //简谱记号
+ *************************************************/
+#define  AS0 29
+#define  B0  31
+#define  C1  33
+#define  CS1 35
+#define  D1  37
+#define  DS1 39
+#define  E1  41
+#define  F1  44
+#define  FS1 46
+#define  G1  49
+#define  GS1 52
+#define  A1  55
+#define  AS1 58
+#define  B1  62
+#define  C2  65
+#define  CS2 69
+#define  D2  73
+#define  DS2 78
+#define  E2  82
+#define  F2  87
+#define  FS2 93
+#define  G2  98
+#define  GS2 104
+#define  A2  110
+#define  AS2 117
+#define  B2  123
+#define  C3  131
+#define  CS3 139
+#define  D3  147
+#define  DS3 156
+#define  E3  165
+#define  F3  175
+#define  FS3 185
+#define  G3  196
+#define  GS3 208
+#define  A3  220
+#define  AS3 233
+#define  B3  247
+#define  C4  262  //*1   
+#define  CS4 277    
+#define  D4  294  //*2
+#define  DS4 311
+#define  E4  330  //*3 蜂鸣器开始清晰
+#define  F4  349  //*4
+#define  FS4 370
+#define  G4  392  //*5
+#define  GS4 415      
+#define  A4  440  //6 
+#define  AS4 466
+#define  B4  494  //7
+#define  C5  523  //1
+#define  CS5 554
+#define  D5  587  //2
+#define  DS5 622
+#define  E5  659  //3
+#define  F5  698  //4
+#define  FS5 740
+#define  G5  784  //5
+#define  GS5 831
+#define  A5  880  //6
+#define  AS5 932
+#define  B5  988  //7
+#define  C6  1047  //1
+#define  CS6 1109
+#define  D6  1175  //2
+#define  DS6 1245
+#define  E6  1319  //3
+#define  F6  1397  //4
+#define  FS6 1480
+#define  G6  1568  //5
+#define  GS6 1661
+#define  A6  1760  //6
+#define  AS6 1865
+#define  B6  1976  //7
+#define  C7  2093  //1
+#define  CS7 2217
+#define  D7  2349  //2
+#define  DS7 2489
+#define  E7  2637  //3
+#define  F7  2794  //4
+#define  FS7 2960
+#define  G7  3136  //5
+#define  GS7 3322  
+#define  A7  3520  //6
+#define  AS7 3729
+#define  B7  3951  //7 #蜂鸣器开始破音
+#define  C8  4186  //1 #
+#define  CS8 4435
+#define  D8  4699  //2 #
+#define  DS8 4978
+#define  A8  7040  //6
